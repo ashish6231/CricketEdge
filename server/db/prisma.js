98df3528b30c89713389827a5c431d0764107d99
Ashish <ashishjaglan6231@gmail.com>
2026-07-30 21:43:23 +0530

    fix: Integrate PrismaPg adapter and enable driverAdapters preview feature

--- server/db/prisma.js
+++ server/db/prisma.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
